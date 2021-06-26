57bb6dc873b045a9b82e6c5a4b9858680a04421f
Amol Chavan <amchavan@mediaocean.com>
2021-06-26 14:54:00 +0530

    Solved Two Sum in array using Map

---
+++ src/TwoSum/ProblemStatement.md
@@ -0,0 +1,10 @@
+<h1> Two Sum</h1>
+<p>
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+</p>
+<p>You may assume that each input would have exactly one solution, and you may not use the same element twice</p>
+<hr>
+Example:
+<br>
+Given: nums = [2, 7, 11, 15], target = 9,
+Because: nums[0] + nums[1] = 2 + 7 = 9, return [0, 1]
---
+++ src/TwoSum/TwoSumSolution1.java
@@ -0,0 +1,29 @@
+package TwoSum;
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumSolution1 {
+
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(nums[i])){
+                return new int[] {map.get(nums[i]),i};
+            }
+            map.put(complement,i);
+        }
+        throw new IllegalArgumentException("No Two Sum Solutions");
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2, 7, 11, 15};
+        int target = 9;
+
+        TwoSumSolution1 solution = new TwoSumSolution1();
+        System.out.println("Solution is =>"+ Arrays.toString(solution.twoSum(array, target)));
+    }
+}
